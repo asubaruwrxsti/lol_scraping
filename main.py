c672c085c8d43469507ba00c4e4ab160f1d1e87d
arlind <arlind12344@gmail.com>
2023-06-19 21:26:15 +0200

    Improvement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Any
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -6,7 +7,7 @@ import json
 import os
 
 # //*[@id="bltce2fa2f4ba0cba4f-section_1"]/section/div[2]/section[2]/div[2]
-url = "https://www.leagueoflegends.com/en-us/champions/"
+main_page_url = "https://www.leagueoflegends.com/en-us/champions/"
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 driver = webdriver.Chrome(options=options)
@@ -14,7 +15,7 @@ driver = webdriver.Chrome(options=options)
 class get_champions():
 
     def populate_champions(self, dump=True):
-        driver.get(url)
+        driver.get(main_page_url)
         champion_wrapper = driver.find_element(By.XPATH, '//*[@id="bltce2fa2f4ba0cba4f-section_1"]/section/div[2]/section[2]/div[2]')
         champions = champion_wrapper.find_elements(By.TAG_NAME, 'a')
         for champion, i in zip(champions, range(len(champions))):
@@ -42,20 +43,39 @@ class get_champions():
             with open('champions.json', 'r') as f:
                 self.scraped_champions = json.load(f)
         
+    def get_champions(self):
         return self.scraped_champions
 
 class get_champion():
 
     def __init__(self, champion):
-        self.champion_name = champion
-        self.champion_url = ""
-        self.champion_stats = {}
-        self.champion_spells = {}
-        self.chamption_spell_images = {}
+        self.champion_name = list(champion.keys())[0]
+        self.champion_url = list(champion.values())[0]
+        self.champion_abilities = {}
+        self.champion_images = {}
     
-    def scrape_champion(self):
+    # //*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]
+    def scrape_champion_abilities(self):
+        driver.get(self.champion_url)
+        abilities = driver.find_elements(By.XPATH, '//*[@id="gatsby-focus-wrapper"]/div/section[2]/div[1]/div[2]/div/div[1]/div/div/div[2]')
+        for ability in abilities:
+            print(ability.text)
+
+    def scrape_champion_images(self):
         pass
 
-if __name__ == '__main__':
-    champions = get_champions()
+    def dump_champion(self):
+        pass
     
+    def scrape_champion(self):
+        driver.get(self.champion_url)
+        self.scrape_champion_abilities()
+        self.scrape_champion_images()
+        self.dump_champion()
+
+if __name__ == '__main__':
+    all_champions = get_champions().get_champions()
+    for champion in all_champions:
+        champion = get_champion(champion)
+        champion.scrape_champion()
+        break
